029ba3cc9653737bcbd8d3e798f280e8400b3811
NeVeS <nevespl@gmail.com>
2022-12-17 16:18:11 +0100

    add sample

---
+++ WebApplication1/user_code.nt.cs
@@ -0,0 +1,12 @@
+﻿using NTypewriter.Editor.Config;
+
+namespace WebApplication1
+{
+    public class user_code : EditorConfig
+    {
+        public static string HelloWorld()
+        {
+            return $"// Hello, from the user code";
+        }
+    }
+}
